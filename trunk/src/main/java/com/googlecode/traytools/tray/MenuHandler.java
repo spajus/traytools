75c50a3867a207b06174a9301f23cb4f44ae962e
tomas.varaneckas <tomas.varaneckas@62f4a426-ed38-44b4-2157-2be4e335d944>
2010-04-10 18:10:54 +0000

    correct popup location on linux

--- trunk/src/main/java/com/googlecode/traytools/tray/MenuHandler.java
+++ trunk/src/main/java/com/googlecode/traytools/tray/MenuHandler.java
@@ -4,11 +4,15 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.TypedEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 
 import com.googlecode.traytools.utils.Logger;
+import com.googlecode.traytools.utils.OSUtils;
 import com.googlecode.traytools.utils.SWTUtils;
+import com.googlecode.traytools.utils.OSUtils.OS;
 
 public class MenuHandler implements ClickHandler {
 	
@@ -29,9 +33,37 @@ public class MenuHandler implements ClickHandler {
 	@Override
 	public void handle(TypedEvent event) {
 		Logger.info("Menu Handler clicked", event);
-
-		menu.setLocation(event.display.getCursorLocation());
+		menu.setLocation(findPopupMenuLocation(event));
 		menu.setVisible(!menu.getVisible());
 	}
+	
+	/**
+     * Finds popup menu location and makes a {@link StateEvent}
+     * 
+     * @return
+     */
+    public static synchronized Point findPopupMenuLocation(TypedEvent event) {
+    	final Display d = event.display;
+    	final Point loc = d.getCursorLocation();
+        int x, y;
+        x = loc.x;
+        y = loc.y;
+        if (!OSUtils.CURRENT_OS.equals(OS.WIN)) {
+            final int traySize = OSUtils.getTraySize();
+            //assume click is in the middle of the icon
+            x = loc.x - traySize / 2;
+            if (loc.y < traySize * 4) {
+                //tray is on top side of the screen
+                y = traySize;
+            } else {
+                //tray is on bottom side of the screen
+                y = d.getBounds().height - traySize;
+            }
+        } else {
+            loc.y -= 2;
+        }
+        return new Point(x, y);
+    }
+
 
 }
